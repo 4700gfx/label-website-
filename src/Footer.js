2e57516c2c3a21e1fbec8777884154767293243e
47gfx <117771075+4700gfx@users.noreply.github.com>
2024-07-22 20:30:16 -0400

    Tightening UI

--- src/Footer.js
+++ src/Footer.js
@@ -13,28 +13,28 @@ const Footer = () => {
         <ul>
           <li><a href="#">Home</a></li>
           <li><a href="#">About Us</a></li>
-          <li><a href="#">Properties</a></li>
-          <li><a href="#">Contact Me</a></li>                                    
+          <li><a href="#">Artist</a></li>
+          <li><a href="#">4700 Store</a></li>                                    
         </ul>
       </div>
 
       <div class="footer-column">
-        <h4>Different Type of Properties</h4>
+        <h4>Services</h4>
         <ul>
-          <li><a href="#">Residental</a></li>
-          <li><a href="#">Commerical</a></li>
-          <li><a href="#">Rural</a></li>
-          <li><a href="#">More Properities</a></li>                                    
+          <li><a href="#">Audio Production</a></li>
+          <li><a href="#">Music Catalog Management</a></li>
+          <li><a href="#">Music Distribution</a></li>
+          <li><a href="#">Music Promotion</a></li>                                    
         </ul>
       </div>
 
 
       <div class="footer-column">
-        <h4>About Me</h4>
+        <h4>Contact Us</h4>
         <ul>
-          <li><a href="#">Blog</a></li>
-          <li><a href="#">Submit Homeowner Form</a></li>
-          <li><a href="#">FAQs</a></li>
+          <li><a href="#">Set An Appointment</a></li>
+          <li><a href="#">Submit Music</a></li>
+          <li><a href="#">Email Us</a></li>
           <li><a href="#">More Information</a></li>                                    
         </ul> 
       </div>
